24107ba85a194ae6b69223ad9876f6f31b46feeb
broken-helix <>
2023-09-12 00:31:53 +0000

    Add image montage to leaves_visualiser page

--- app_pages/summary.py
+++ app_pages/summary.py
@@ -13,7 +13,7 @@ def summary_body():
 
     st.success(
         f"### The Disease\n\n"
-        f"Powdery Mildew is a disease is caused by Podosphaera clandestina, "
+        f"Powdery Mildew is a disease is caused by *Podosphaera clandestina*, "
         f"one of the common species of the powdery mildew group of fungi.\n\n"
         f"The disease affects cherry trees, damages and stunts new growth "
         f"and can affect crop return in commercial settings.\n\n"
@@ -52,13 +52,3 @@ def summary_body():
         f"2104 infected leaves."
     )
     st.write("---")
-
-
-"""
-The cherry plantation crop from Farmy & Foods is facing a challenge where their cherry plantations have been presenting powdery mildew. Currently, the process is manual verification if a given cherry tree contains powdery mildew. An employee spends around 30 minutes in each tree, taking a few samples of tree leaves and verifying visually if the leaf tree is healthy or has powdery mildew. If there is powdery mildew, the employee applies a specific compound to kill the fungus. The time spent applying this compound is 1 minute. The company has thousands of cherry trees, located on multiple farms across the country. As a result, this manual process is not scalable due to the time spent in the manual process inspection.
-
-To save time in this process, the IT team suggested an ML system that detects instantly, using a leaf tree image, if it is healthy or has powdery mildew. A similar manual process is in place for other crops for detecting pests, and if this initiative is successful, there is a realistic chance to replicate this project for all other crops. The dataset is a collection of cherry leaf images provided by Farmy & Foods, taken from their crops.
-
-1 - The client is interested in conducting a study to visually differentiate a healthy cherry leaf from one with powdery mildew.
-2 - The client is interested in predicting if a cherry leaf is healthy or contains powdery mildew.
-"""
