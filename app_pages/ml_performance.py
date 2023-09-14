c1536c16560158062073f76b25c1ce08c3b76303
broken-helix <>
2023-09-14 12:03:23 +0000

    Add content to ML Performance page

--- app_pages/ml_performance.py
+++ app_pages/ml_performance.py
@@ -1,7 +1,42 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.image import imread
+from src.machine_learning.evaluate_clf import load_test_evaluation
 
 
 def ml_performance_body():
-
     st.write("ML Performance")
     st.write("---")
+
+    version = 'v3'
+
+    st.write("### Train, Validation and Test Set: Labels Frequencies")
+
+    labels_distribution = plt.imread(
+                          f"outputs/{version}/labels_distribution.png")
+    st.image(labels_distribution,
+             caption='Labels Distribution on Train, Validation and Test Sets')
+    st.write("---")
+
+    labels_distribution = plt.imread(
+                          f"outputs/{version}/sets_distribution_pie.png")
+    st.image(labels_distribution, caption='Sets distribution')
+
+
+    st.write("### Model History")
+    col1, col2 = st.beta_columns(2)
+    with col1: 
+        model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
+        st.image(model_acc, caption='Model Training Accuracy')
+    with col2:
+        model_loss = plt.imread(f"outputs/{version}/model_training_losses.png")
+        st.image(model_loss, caption='Model Training Losses')
+    st.write("---")
+
+    model_cm = plt.imread(f"outputs/{version}/confusion_matrix.png")
+    st.image(model_cm, caption='Confusion Matrix')
+
+    st.write("### Generalised Performance on Test Set")
+    st.dataframe(pd.DataFrame(load_test_evaluation(version),
+                              index=['Loss', 'Accuracy']))
